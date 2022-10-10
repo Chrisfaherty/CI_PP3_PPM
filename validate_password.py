8681e0c240a62dcebff607880d9c536243115728
Chrisfaherty <chrismactom@gmail.com>
2022-10-10 15:42:26 +0000

    Resolve errors on validate_password.py file

--- validate_password.py
+++ validate_password.py
@@ -1,38 +1,35 @@
 def validate_password(password_to_validate):
-            """
-            Inside the try, checks to make sure the password
-            contains a capital letter,
-            lowercase letter & a special character.
-            Raises ValueError if the password does not contain 
-            a lowercase & uppercase,
-            or if there aren't more than 6 values. 
-            credits geeksforgeeks.org
-            """
-            l, u, p, d = 0, 0, 0, 0
-            s = password_to_validate
-            capitalalphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            smallalphabets = "abcdefghijklmnopqrstuvwxyz"
-            specialchar = "$@_"
-            digits = "0123456789"
-            if (len(s) >= 8):
-                for i in s:
-            
-                    # counting lowercase alphabets
-                    if (i in smallalphabets):
-                        l += 1           
-            
-                    # counting uppercase alphabets
-                    if (i in capitalalphabets):
-                        u += 1           
-            
-                    # counting digits
-                    if (i in digits):
-                        d += 1           
-            
-                    # counting the mentioned special characters
-                    if(i in specialchar):
-                        p += 1       
-            if (l >= 1 and u >= 1 and p >= 1 and d >= 1 and l + p + u + d == len(s)):
+    """
+    Inside the try, checks to make sure the password
+    contains a capital letter,
+    lowercase letter & a special character.
+    Raises ValueError if the password does not contain 
+    a lowercase & uppercase,
+    or if there aren't more than 6 values. 
+    credits geeksforgeeks.org
+    """
+    lower, upper, special, digit = 0, 0, 0, 0
+    s = password_to_validate
+    capitalalphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    smallalphabets = "abcdefghijklmnopqrstuvwxyz"
+    specialchar = "$@_"
+    digits = "0123456789"
+    if len(s) >= 8:
+        for i in s:
+            # counting lowercase alphabets
+            if i in smallalphabets:
+                lower += 1           
+            # counting uppercase alphabets
+            if i in capitalalphabets:
+                upper += 1           
+            # counting digits
+            if i in digits:
+                digit += 1           
+            # counting the mentioned special characters
+            if i in specialchar:
+                special += 1       
+            if lower >= 1 and upper >= 1 and special >= 1 and \
+                    digit >= 1 and lower + special + upper + digit == len(s):
                 print("Valid Password")
             else:
                 print("Invalid Password")
