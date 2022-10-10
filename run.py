4ed0471d2c9bd5547f7f1e1a1bce9dad5fe6c4a9
Chrisfaherty <chrismactom@gmail.com>
2022-10-10 20:15:07 +0000

    Create a way for the user to return to the create account or login option if they already have an account created

--- run.py
+++ run.py
@@ -26,135 +26,143 @@ print("Hi " + name + ", Welcome to your personal password manager!")
 
 # This if statemant will be used for the log in screen to see if
 # they want to create an account or login to an existing account.
+def main ():
+    """
+    This is the main function to allow me to return to it.
+    """
+    answer = input("Create account or Login ").lower()
 
-answer = input("Create account or Login ").lower()
-
-if answer == "create account":
-    from validate_password import validate_password
-    
-    def create_master_account():
-        """
-        Create the master pwd and username for your
-        personal pwd manager account
-        """
-        master_account_username = input("Create your manager username: ")
-        print(f"Storing username {master_account_username} ...\n")
-
-        while True:
-            master_account_password = input("create your master password: ")
-            if validate_password(master_account_password):
-                password_to_validate = master_account_password
-                validate_password(password_to_validate)
-                print(f"Storing password {master_account_password} ...\n")
-                break
-
-        return name, master_account_username, master_account_password
-
-    def update_settings_worksheet(master_data):
-        """Update sales worksheet with the master data"""
-        print("Updating settings worksheet ...\n")
-        settings_worksheet = SHEET.worksheet('settings')
-        settings_worksheet.append_row(master_data)
-        print("Settings updated sucessfully.\n")
-    master_data = create_master_account()
-    print(master_data)
-    update_settings_worksheet(master_data)
-
-# The functions below here will be for the steps
-# that will occure once logged in.
-elif answer == "login":
-    login_username = input('Input your username: ')
-    login_password = input('Input your password: ')
-
-    settings_worksheet = SHEET.worksheet('settings')
-    actual_username = settings_worksheet.acell('B2').value
-    actual_password = settings_worksheet.acell('C2').value
-    print(actual_password)
-    print(actual_username)
-
-    if login_username == actual_username and login_password == actual_password:
-        print("login.....")
-    else:
-        print("Username and password didn't match our records")
-
-    print("Type 1 to View, 2 to add, 3 to edit & 4 to edit master pwd")
-
-    option = input('Type 1, 2, 3 or 4: ')
-
-    if option == "1":
-        print("You selected to view your passwords")
-
-        def view_passwords():
-            """
-            This Function is used to pull the passwords from
-            the database and display them in the terminal.
-            """
-            print("Retriving passwords!... ")
-            all_passwords = SHEET.worksheet("password_manager").get_all_values(
-            )
-            pprint.pprint(all_passwords)
-        view_passwords()
-
-    elif option == "2":
+    if answer == "create account":
         from validate_password import validate_password
-        print("You selected to add a new password")
-
-        def store_password():
+        
+        def create_master_account():
             """
-            store a new website, username & password into the database.
+            Create the master pwd and username for your
+            personal pwd manager account
             """
-            new_website = input("Input the website: ").lower()
-            print(f"Storing website {new_website} ...\n")
-
-            new_username = input("Input your username: ")
-            print(f"Storing username {new_username} ...\n")
-
+            settings_worksheet = SHEET.worksheet('settings')
+            check_for_account = settings_worksheet.acell('B2').value
             while True:
-                new_password = input("Input your password: ")
-                if validate_password(new_password):
-                    password_to_validate = new_password
-                    validate_password(password_to_validate)
-                    print(f"Storing password {new_password} ...\n")
-                    break
+                if check_for_account is None:
+                    master_account_username = input("Create your manager username: ")
+                    print(f"Storing username {master_account_username} ...\n")
+                    while True:
+                        master_account_password = input("create your master password: ")
+                        if validate_password(master_account_password):
+                            password_to_validate = master_account_password
+                            validate_password(password_to_validate)
+                            print(f"Storing password {master_account_password} ...\n")
+                            break
 
-            return new_website, new_username, new_password
+                    return name, master_account_username, master_account_password
+                print("Account already set up try log in")
+                main()
 
-        def update_password_manager_worksheet(new_data):
-            """Update password_manager worksheet with the master data"""
-            print("Updating password_manager worksheet ...\n")
-            password_manager_worksheet = SHEET.worksheet('password_manager')
-            password_manager_worksheet.append_row(new_data)
-            print("password manager updated sucessfully.\n")
+        def update_settings_worksheet(master_data):
+            """Update sales worksheet with the master data"""
+            settings_worksheet = SHEET.worksheet('settings')
+            settings_worksheet.append_row(master_data)
+        master_data = create_master_account()
+        print(master_data)
+        update_settings_worksheet(master_data)
 
-        new_data = store_password()
-        print(new_data)
-        update_password_manager_worksheet(new_data)
+    # The functions below here will be for the steps
+    # that will occure once logged in.
+    elif answer == "login":
+        login_username = input('Input your username: ')
+        login_password = input('Input your password: ')
 
-    elif option == "3":
-        print("You selected to edit a password")
-        find_account = input("Account you would like to edit the password: ")
-        password_manager_worksheet = SHEET.worksheet('password_manager')
-        cell_of_account = str(password_manager_worksheet.find(find_account))
-        column_of_account = int(cell_of_account[9]) + 2
-        row_of_account = cell_of_account[7]
-        new_password = input("input your new password: ")
-        update_cell_of_account = password_manager_worksheet.update_cell(
-            row_of_account, column_of_account, new_password)
-
-    elif option == "4":
-        print("You selected to edit master password")
-
-        find_account = input("Account you would like to edit the password: ")
         settings_worksheet = SHEET.worksheet('settings')
-        cell_of_account = str(settings_worksheet.find(find_account))
-        column_of_account = int(cell_of_account[9]) + 2
-        row_of_account = cell_of_account[7]
-        new_password = input("input your new password: ")
-        update_cell_of_account = settings_worksheet.update_cell(
-            row_of_account, column_of_account, new_password)
+        actual_username = settings_worksheet.acell('B2').value
+        actual_password = settings_worksheet.acell('C2').value
+        print(actual_password)
+        print(actual_username)
+
+        if login_username == actual_username and login_password == actual_password:
+            print("login.....")
+        else:
+            print("Username and password didn't match our records")
+
+        print("Type 1 to View, 2 to add, 3 to edit & 4 to edit master pwd")
+
+        option = input('Type 1, 2, 3 or 4: ')
+
+        if option == "1":
+            print("You selected to view your passwords")
+
+            def view_passwords():
+                """
+                This Function is used to pull the passwords from
+                the database and display them in the terminal.
+                """
+                print("Retriving passwords!... ")
+                all_passwords = SHEET.worksheet("password_manager").get_all_values(
+                )
+                pprint.pprint(all_passwords)
+            view_passwords()
+
+        elif option == "2":
+            from validate_password import validate_password
+            print("You selected to add a new password")
+
+            def store_password():
+                """
+                store a new website, username & password into the database.
+                """
+                new_website = input("Input the website: ").lower()
+                print(f"Storing website {new_website} ...\n")
+
+                new_username = input("Input your username: ")
+                print(f"Storing username {new_username} ...\n")
+
+                while True:
+                    new_password = input("Input your password: ")
+                    if validate_password(new_password):
+                        password_to_validate = new_password
+                        validate_password(password_to_validate)
+                        print(f"Storing password {new_password} ...\n")
+                        break
+
+                return new_website, new_username, new_password
+
+            def update_password_manager_worksheet(new_data):
+                """Update password_manager worksheet with the master data"""
+                print("Updating password_manager worksheet ...\n")
+                password_manager_worksheet = SHEET.worksheet('password_manager')
+                password_manager_worksheet.append_row(new_data)
+                print("password manager updated sucessfully.\n")
+
+            new_data = store_password()
+            print(new_data)
+            update_password_manager_worksheet(new_data)
+
+        elif option == "3":
+            print("You selected to edit a password")
+            find_account = input("Account you would like to edit the password: ")
+            password_manager_worksheet = SHEET.worksheet('password_manager')
+            cell_of_account = str(password_manager_worksheet.find(find_account))
+            column_of_account = int(cell_of_account[9]) + 2
+            row_of_account = cell_of_account[7]
+            new_password = input("input your new password: ")
+            update_cell_of_account = password_manager_worksheet.update_cell(
+                row_of_account, column_of_account, new_password)
+
+        elif option == "4":
+            print("You selected to edit master password")
+
+            find_account = input("Account you would like to edit the password: ")
+            settings_worksheet = SHEET.worksheet('settings')
+            cell_of_account = str(settings_worksheet.find(find_account))
+            column_of_account = int(cell_of_account[9]) + 2
+            row_of_account = cell_of_account[7]
+            new_password = input("input your new password: ")
+            update_cell_of_account = settings_worksheet.update_cell(
+                row_of_account, column_of_account, new_password)
+
+        else:
+            print("You did not enter a valid response!. ")
 
     else:
         print("You did not enter a valid response!. ")
 
-else:
-    print("You did not enter a valid response!. ")
+main()
