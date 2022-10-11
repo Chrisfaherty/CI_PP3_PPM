3c39d9d9ed149e00b48e484f0d30560bc7718cd2
Chrisfaherty <chrismactom@gmail.com>
2022-10-11 14:12:12 +0000

    Fix the bug that allows the user to enter the same website more than once

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('ci_pp3_ppm')
 
 # welcome message
-name = input("Input your name: ")
+name = input("Input your name: ").lower()
 print("Hi " + name + ", Welcome to your personal password manager!")
 
 # This if statemant will be used for the log in screen to see if
@@ -83,18 +83,17 @@ def main():
         settings_worksheet = SHEET.worksheet('settings')
         actual_username = settings_worksheet.acell('B2').value
         actual_password = settings_worksheet.acell('C2').value
-        print(actual_password)
-        print(actual_username)
         if login_username == actual_username\
                 and login_password == actual_password:
             print("login.....")
         else:
             print("Username and password didn't match our records")
+            main()
 
         def options():
             """function used to return to the option input"""
             print("Type 1 to View, 2 to add, 3 to edit,\
-            4 to edit master pwd & 5 to exit")
+4 to edit master pwd & 5 to exit")
             option = input('Type 1, 2, 3, 4 or 5: ')
             if option == "1":
                 print("You selected to view your passwords")
@@ -110,50 +109,62 @@ def main():
                     pprint.pprint(all_passwords)
                 view_passwords()
                 options()
-            
+
             elif option == "2":
                 from validate_password import validate_password
                 print("You selected to add a new password")
+                new_website = input("Input the website: ").lower()
+                password_manager_worksheet = SHEET.worksheet(
+                            'password_manager')
+                current_stored_website = password_manager_worksheet.find(
+                    new_website)
+                if str(new_website) in str(current_stored_website):
+                    print(f"{new_website} already exsists.")
+                    print('Try adding a digit aftwer the name to \
+differenciate it from the others')
+                    options()
 
-                def store_password():
-                    """
-                    store a new website, username & password into the database.
-                    """
-                    new_website = input("Input the website: ").lower()
-                    print(f"Storing website {new_website} ...\n")
-                    new_username = input("Input your username: ")
-                    print(f"Storing username {new_username} ...\n")
+                else:
 
-                    while True:
-                        new_password = input("Input your password: ")
-                        if validate_password(new_password):
-                            password_to_validate = new_password
-                            validate_password(password_to_validate)
-                            print(f"Storing password {new_password} ...\n")
-                            break
+                    def store_password():
+                        """
+                        store a new website, username & password 
+                        into the database.
+                        """
+                        
+                        print(f"Storing website {new_website} ...\n")
+                        new_username = input("Input your username: ")
+                        print(f"Storing username {new_username} ...\n")
 
-                    return new_website, new_username, new_password
+                        while True:
+                            new_password = input("Input your password: ")
+                            if validate_password(new_password):
+                                password_to_validate = new_password
+                                validate_password(password_to_validate)
+                                print(f"Storing password {new_password} ...\n")
+                                break
+                        return new_website, new_username, new_password
 
-                def update_password_manager_worksheet(new_data):
-                    """
-                    Update password_manager worksheet
-                    with the master data
-                    """
-                    print("Updating password_manager worksheet ...\n")
-                    password_manager_worksheet = SHEET.worksheet(
-                        'password_manager')
-                    password_manager_worksheet.append_row(new_data)
-                    print("password manager updated sucessfully.\n")
+                    def update_password_manager_worksheet(new_data):
+                        """
+                        Update password_manager worksheet
+                        with the master data
+                        """
+                        print("Updating password_manager worksheet ...\n")
+                        password_manager_worksheet = SHEET.worksheet(
+                            'password_manager')
+                        password_manager_worksheet.append_row(new_data)
+                        print("password manager updated sucessfully.\n")
 
-                new_data = store_password()
-                print(new_data)
-                update_password_manager_worksheet(new_data)
-                options()
+                    new_data = store_password()
+                    print(new_data)
+                    update_password_manager_worksheet(new_data)
+                    options()
 
             elif option == "3":
                 print("You selected to edit a password")
                 find_account = input("Account you would like to edit\
-                 the password: ")
+the password: ").lower()
                 password_manager_worksheet = SHEET.worksheet(
                     'password_manager')
                 cell_of_account = str(
@@ -168,8 +179,8 @@ def main():
 
             elif option == "4":
                 print("You selected to edit master password")
-                find_account = input("Account you would like\
-                 to edit the password: ")
+                find_account = input("Account name you would like\
+to edit the password: ").lower()
                 settings_worksheet = SHEET.worksheet('settings')
                 cell_of_account = str(settings_worksheet.find(find_account))
                 column_of_account = int(cell_of_account[9]) + 2
