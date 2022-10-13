58a1bce9630871a8b4c6409fd9047d3bfa22b6c4
Chrisfaherty <chrismactom@gmail.com>
2022-10-13 17:59:04 +0000

    Edit logo to remove errors in the terminal for being to long

--- run.py
+++ run.py
@@ -28,15 +28,24 @@ def logo():
     print(" ")
     print(Fore.BLUE + "Welcome to:")
     print(" ")
-    print(Fore.GREEN + " ____________________________________________________________________________________________________ ")
-    print(Fore.GREEN + "|    ____       ___          ___        ___    __                  __     ___     _____      ____    |")
-    print(Fore.GREEN + "|   / __ \     /   \        /    \     /    \  \ \                / /   / __ \   |  __  \   |  _ \   |")
-    print(Fore.GREEN + "|  | /  \ |   /  _  \      /  /\__\   /  /\__\  \ \      __      / /   / /  \ \  | |  | |   | | \ \  |")
-    print(Fore.GREEN + "|  | \__/ |  /  /_\  \     \  \       \  \       \ \    /  \    / /   | |    | | | |__| |   | |  | | |")
-    print(Fore.GREEN + "|  |  ___/  /   ___   \   __ \  \   __ \  \       \ \  / /\ \  / /    | |    | | |  __  \   | |  | | |")
-    print(Fore.GREEN + "|  | |     /  /     \  \  \ \/  /   \ \/  /        \ \/ /  \ \/ /      \ \__/ /  | |  \  \  | |_/ /  |")
-    print(Fore.GREEN + "|  |_|    /__/       \__\  \___/     \___/          \__/    \__/        \____/   |_|   \__\ |____/   |")
-    print(Fore.GREEN + "|____________________________________________________________________________________________________|")
+    print(Fore.GREEN + " _____________________________________________\
+_______________________________________________________ ")
+    print(Fore.GREEN + "|    ____       ___          ___        ___   \
+__                  __     ___     _____      ____    |")
+    print(Fore.GREEN + "|   / __ \     /   \        /    \     /    \ \
+\ \                / /   / __ \   |  __  \   |  _ \   |")
+    print(Fore.GREEN + "|  | /  \ |   /  _  \      /  /\__\   /  /\_\ \
+ \ \      __      / /   / /  \ \  | |  | |   | | \ \  |")
+    print(Fore.GREEN + "|  | \__/ |  /  /_\  \     \  \       \  \    \
+  \ \    /  \    / /   | |    | | | |__| |   | |  | | |")
+    print(Fore.GREEN + "|  |  ___/  /   ___   \   __ \  \   __ \  \   \
+   \ \  / /\ \  / /    | |    | | |  __  \   | |  | | |")
+    print(Fore.GREEN + "|  | |     /  /     \  \  \ \/  /   \ \/  /   \
+    \ \/ /  \ \/ /      \ \__/ /  | |  \  \  | |_/ /  |")
+    print(Fore.GREEN + "|  |_|    /__/       \__\  \___/     \___/    \
+     \__/    \__/        \____/   |_|   \__\ |____/   |")
+    print(Fore.GREEN + "|_____________________________________________\
+______________________________________________________|")
     print(" ")
     print(" ")
     print(Fore.BLUE + "Your Personal Password Manager")
@@ -80,15 +89,13 @@ def view_passwords():
     the database and display them in the terminal.
     """
     print("Retriving passwords!... ")
-    all_passwords = SHEET.worksheet(
-        "password_manager").get_all_values()
+    all_passwords = SHEET.worksheet("password_manager").get_all_values()
     return all_passwords
 
 
 def add_passwords():
     new_website = input("Input the website: ").lower()
-    password_manager_worksheet = SHEET.worksheet(
-        'password_manager')
+    password_manager_worksheet = SHEET.worksheet('password_manager')
     current_stored_website = password_manager_worksheet.find(
         new_website)
     return new_website, current_stored_website
@@ -106,10 +113,8 @@ def update_password_manager_worksheet(new_data):
 
 
 def edit_passwords():
-    find_account = input("Account you would like to edit \
-the password: ").lower()
-    password_manager_worksheet = SHEET.worksheet(
-        'password_manager')
+    find_account = input("Account's password to edit: ").lower()
+    password_manager_worksheet = SHEET.worksheet('password_manager')
     cell_of_account = str(
         password_manager_worksheet.find(find_account))
     if (cell_of_account[8]) == 'C':
@@ -118,20 +123,19 @@ the password: ").lower()
     else:
         column_of_account = int(cell_of_account[10]) + 2
         row_of_account = cell_of_account[7:9]
-    new_password = input("input your new password: ")
+    new_password = input("New password: ")
     # update cell of account
     password_manager_worksheet.update_cell(
         row_of_account, column_of_account, new_password)
 
 
 def edit_master_password():
-    find_account = input("Account name you would like\
-to edit the password: ").lower()
+    find_account = input("Enter your name ").lower()
     settings_worksheet = SHEET.worksheet('settings')
     cell_of_account = str(settings_worksheet.find(find_account))
     column_of_account = int(cell_of_account[9]) + 2
     row_of_account = cell_of_account[7]
-    new_password = input("input your new password: ")
+    new_password = input("New password: ")
     settings_worksheet.update_cell(
         row_of_account, column_of_account, new_password)
     
@@ -177,15 +181,14 @@ def main():
             """Update ssettings worksheet with the master data"""
             settings_worksheet = SHEET.worksheet('settings')
             settings_worksheet.append_row(master_data)
+
         master_data = create_master_account()
         print(master_data)
         update_settings_worksheet(master_data)
 
-    # The functions below here will be for the steps
-    # that will occure once logged in.
     elif answer == "login":
-        login_username, login_password, actual_username,\
-            actual_password = login()
+        login_username, login_password, actual_username, actual_password\
+            = login()
         if login_username == actual_username\
                 and login_password == actual_password:
             print("login.....")
@@ -195,22 +198,21 @@ def main():
 
         def options():
             """function used to return to the option input"""
-            print("Type 1 to View, 2 to add, 3 to edit,\
-4 to edit master pwd & 5 to exit")
+            print("Type 1: View, 2: Add, 3: Edit, 4: edit master pwd, 5: exit")
             option = input('Type 1, 2, 3, 4 or 5: ')
             if option == "1":
-                print("You selected to view your passwords")
+                print("Viewing your passwords")
                 all_passwords = view_passwords()
                 pprint.pprint(all_passwords)
                 options()
 
             elif option == "2":
-                print("You selected to add a new password")
+                print("Adding a new password")
                 new_website, current_stored_website = add_passwords()
                 if str(new_website) in str(current_stored_website):
                     print(f"{new_website} already exsists.")
-                    print('Try adding a digit aftwer the name to \
-differenciate it from the others')
+                    print('Try adding a digit after the name')
+                    print('Make it different to the others')
                     options()
 
                 else:
@@ -220,37 +222,36 @@ differenciate it from the others')
                         store a new website, username & password 
                         into the database.
                         """
-                        
                         print(f"Storing website {new_website} ...\n")
-                        new_username = input("Input your username: ")
+                        new_username = input("Input username: ")
                         print(f"Storing username {new_username} ...\n")
 
                         while True:
-                            new_password = input("Input your password: ")
+                            new_password = input("Input password: ")
                             password_to_validate = new_password
                             if validate_password(password_to_validate):
                                 print(f"Storing password \
                                     {new_password} ...\n")
                                 break
                         return new_website, new_username, new_password
-
+           
                     new_data = store_password()
                     update_password_manager_worksheet(new_data)
                     print(new_data)
                     options()
 
             elif option == "3":
-                print("You selected to edit a password")
+                print("Editing a password")
                 edit_passwords()
                 options()
 
             elif option == "4":
-                print("You selected to edit master password")
+                print("Editing master password")
                 edit_master_password()
                 options()
 
             elif option == "5":
-                print("you have selected to log it. Thank you, Good Bye!")
+                print("Loging out. Thank you, Good Bye!")
                 main()
             else:
                 print("You did not enter a valid response!. ")
