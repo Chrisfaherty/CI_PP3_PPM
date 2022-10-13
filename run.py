8e0d90e6519259b2f65649f7468d400e24c613bc
Chrisfaherty <chrismactom@gmail.com>
2022-10-13 19:02:06 +0000

    Edit all inputs to allow space before the sentance on the app

--- run.py
+++ run.py
@@ -47,7 +47,7 @@ def logo():
 
 logo()
 # welcome message
-name = input("Input your name: \n ").lower()
+name = input(" Input your name: \n ").lower()
 print("Hi " + name + ", Welcome to your personal password manager!")
 
 # This if statemant will be used for the log in screen to see if
@@ -55,7 +55,7 @@ print("Hi " + name + ", Welcome to your personal password manager!")
 
 
 def setup() -> str:
-    answer = input("Create account or Login:\n").lower()
+    answer = input(" Create account or Login: \n ").lower()
     return answer
 
 
@@ -66,8 +66,8 @@ def check_for_master_account():
 
 
 def login():
-    login_username = input('Input your username:\n ')
-    login_password = input('Input your password:\n ')
+    login_username = input(' Input your username: \n ')
+    login_password = input(' Input your password: \n ')
     settings_worksheet = SHEET.worksheet('settings')
     actual_username = settings_worksheet.acell('B2').value
     actual_password = settings_worksheet.acell('C2').value
@@ -85,7 +85,7 @@ def view_passwords():
 
 
 def add_passwords():
-    new_website = input("Input the website:\n ").lower()
+    new_website = input(" Input the website: \n ").lower()
     password_manager_worksheet = SHEET.worksheet('password_manager')
     current_stored_website = password_manager_worksheet.find(
         new_website)
@@ -104,7 +104,7 @@ def update_password_manager_worksheet(new_data):
 
 
 def edit_passwords():
-    find_account = input("Account's password to edit:\n ").lower()
+    find_account = input(" Account's password to edit: \n ").lower()
     password_manager_worksheet = SHEET.worksheet('password_manager')
     cell_of_account = str(
         password_manager_worksheet.find(find_account))
@@ -114,19 +114,19 @@ def edit_passwords():
     else:
         column_of_account = int(cell_of_account[10]) + 2
         row_of_account = cell_of_account[7:9]
-    new_password = input("New password:\n ")
+    new_password = input(" New password: \n ")
     # update cell of account
     password_manager_worksheet.update_cell(
         row_of_account, column_of_account, new_password)
 
 
 def edit_master_password():
-    find_account = input("Enter your name:\n ").lower()
+    find_account = input(" Enter your name: \n ").lower()
     settings_worksheet = SHEET.worksheet('settings')
     cell_of_account = str(settings_worksheet.find(find_account))
     column_of_account = int(cell_of_account[9]) + 2
     row_of_account = cell_of_account[7]
-    new_password = input("New password:\n ")
+    new_password = input(" New password: \n ")
     settings_worksheet.update_cell(
         row_of_account, column_of_account, new_password)
     
@@ -148,12 +148,12 @@ def main():
             check_for_account = check_for_master_account()
             while True:
                 if check_for_account is None:
-                    master_account_username = input("Create your\
-                        manager username:\n ")
+                    master_account_username = input(" Create your\
+                        manager username: \n ")
                     print(f"Storing username {master_account_username} ...\n")
                     while True:
-                        master_account_password = input("create your\
-                             master password:\n ")
+                        master_account_password = input(" Create your\
+                             master password: \n ")
                         password_to_validate = master_account_password
                         if validate_password(password_to_validate):
                             print(f"Storing password \
@@ -169,7 +169,7 @@ def main():
                 main()
 
         def update_settings_worksheet(master_data):
-            """Update ssettings worksheet with the master data"""
+            """Update settings worksheet with the master data"""
             settings_worksheet = SHEET.worksheet('settings')
             settings_worksheet.append_row(master_data)
 
@@ -190,7 +190,7 @@ def main():
         def options():
             """function used to return to the option input"""
             print("Type 1: View, 2: Add, 3: Edit, 4: edit master pwd, 5: exit")
-            option = input('Type 1, 2, 3, 4 or 5:\n ')
+            option = input( 'Type 1, 2, 3, 4 or 5: \n ')
             if option == "1":
                 print("Viewing your passwords")
                 all_passwords = view_passwords()
@@ -218,7 +218,7 @@ def main():
                         print(f"Storing username {new_username} ...\n")
 
                         while True:
-                            new_password = input("Input password: ")
+                            new_password = input(" Input password: \n ")
                             password_to_validate = new_password
                             if validate_password(password_to_validate):
                                 print(f"Storing password \
